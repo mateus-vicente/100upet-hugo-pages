f33e16d86c5dd999fcb6027024b0f85c9357fd1a
Mateus Vicente <mvicente@cern.ch>
2023-09-15 23:56:57 +0200

    going back to js intead of jsm

--- content/en/main.js
+++ content/en/main.js
@@ -1,6 +1,7 @@
-import * as THREE from 'three';
+//import * as THREE from 'three';
 //import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+//import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 document.addEventListener('DOMContentLoaded', function () {
 	// Define variables
@@ -29,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,	10,1000);
 		//camera = new THREE.OrthographicCamera(window.innerWidth, window.innerWidth window.innerHeight, window.innerHeight, 100, 100000);
-		camera.position.set(-120, 0, 0);
+		camera.position.set(0, 0, 130);
 		//camera.lookAt(new THREE.Vector3(0, -100, 0));
 		//camera.zoom = 0.5;
 
@@ -38,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		document.getElementById('scene-container').appendChild(renderer.domElement);
 
-		//const controls = new OrbitControls(camera, renderer.domElement);
+		//const controls = new THREE.OrbitControls(camera, renderer.domElement);
 		//controls.enableZoom = false;
 
 		const light1 = new THREE.AmbientLight(0xffffff, 1); // soft white light
@@ -46,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		scene.add(light1);
 		scene.add(light2);
 
-		const loader = new GLTFLoader();
+		const loader = new THREE.GLTFLoader();
 		loader.load( 'tower_w_2blocks.gltf', function ( gltf ) {
 			gltf.scene.traverse(child => {
 							if (child.isMesh) {
@@ -62,9 +63,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			});
 			///*
 			gltf.scene.scale.setScalar(1); // Scaling is set to 1000 to go from mm to 100 µm
-			gltf.scene.rotation.set(0,0.5*Math.PI,0);
-			gltf.scene.translateX(16.2);
-			gltf.scene.translateY(18.15);
+			gltf.scene.rotation.set(0,Math.PI,0);
+			//gltf.scene.translateX(16.2);
+			//gltf.scene.translateY(18.15);
 
 
 			scannerobj = gltf.scene;
@@ -72,20 +73,20 @@ document.addEventListener('DOMContentLoaded', function () {
 			scannerobj2 = gltf.scene.clone();
 			scannerobj3 = gltf.scene.clone();
 
-			scannerobj.translateZ(100);
-			scannerobj.translateY(100);
+			scannerobj.translateX(19);
+			scannerobj.translateY(200);
 
 			scannerobj1.translateY(-300);
-			scannerobj1.translateX(-32.4);
-			scannerobj1.rotation.set(Math.PI,0.5*Math.PI,0);
+			scannerobj1.translateX(100);
+			scannerobj1.rotation.set(Math.PI,0,0);
 
 			scannerobj2.translateX(-300);
-			scannerobj2.translateY(-21);
-			scannerobj2.rotation.set(Math.PI,0.5*Math.PI,-0.5*Math.PI);
+			scannerobj2.translateY(0);
+			scannerobj2.rotation.set(Math.PI,0,-0.5*Math.PI);
 
 			scannerobj3.translateX(300);
 			scannerobj3.translateY(0);
-			scannerobj3.rotation.set(Math.PI,0.5*Math.PI,0.5*Math.PI);
+			scannerobj3.rotation.set(Math.PI,0,0.5*Math.PI);
 
 			scene.add(scannerobj);
 			scene.add(scannerobj1);
@@ -97,13 +98,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			scanner_anim_tl
 				.to(".square", { y: -370}, "test")
-				.to(camera.position, { x: -100}, "test")
-				.to(scannerobj.position,  {  x: 0, y: 19, z: -19 }, "test") 
-				.to(scannerobj1.position, {  x: 0, y: -19, z: 19 }, "test") 
-				.to(scannerobj2.position, {  x: 0, y: 19, z: 19 }, "test") 
-				.to(scannerobj3.position, {  x: 0, y: -19, z: -19 }, "test") 
-				.to(camera.position, { z: 50 }, "test2") 
-				.to(".square", { x: -900, y: -370}, "test2")
+				.to(camera.position, { z: 100}, "test")
+				.to(scannerobj.position,  {  z: 0, y: 19, x: -19 }, "test") 
+				.to(scannerobj1.position, {  z: 0, y: -19, x: 19 }, "test") 
+				.to(scannerobj2.position, {  z: 0, y: 19, x: 19 }, "test") 
+				.to(scannerobj3.position, {  z: 0, y: -19, x: -19 }, "test") 
+				.to(camera.position, { x: 50, z: 100 }, "test2") 
+				.to(".square", { left: "50%", y: -370}, "test2")
 				.to(scene.rotation, { y: Math.PI/4 }, "test2")
 				; 
 
@@ -137,8 +138,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	let scanner_anim_tl = gsap.timeline({
 		scrollTrigger: {
 			trigger: ".container",
-			start: "50% 100%", 
-			end: "bottom bottom", 
+			start: "15% 100%", 
+			end: "95% 100%", 
 			markers: false,
 			scrub: 1, 
 		}
